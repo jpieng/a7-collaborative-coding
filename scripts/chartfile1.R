4060ecb19da0bfa7f20839319dd870a7f56b9b15
EPAHANG <epahang@uw.edu>
2016-02-20 02:01:07 -0800

    Move chartfile1.r under scripts folder

---
+++ scripts/chartfile1.R
@@ -0,0 +1,11 @@
+# Assignment 7 - Chart File 1
+
+# Install packages
+library(jsonlite)
+library(dplyr)
+library(plotly)
+
+# Write a function that takes in a dataset as a parameter, and returns a visualization 
+# of that data. This will allow you to call your charting function from your index.Rmd file, 
+# keeping your code much easier to read.
+
