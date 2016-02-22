53039bab64939d7877d5334a74ffeca989fad7ae
Jonathan Ieng <Jonathan Ieng>
2016-02-22 11:18:37 -0800

    chartfile2 function added to index.rmd and is functional. Close#4

--- scripts/chartfile2.R
+++ scripts/chartfile2.R
@@ -11,20 +11,20 @@ make_bar_chart <- function (the_data) {
   # Creates new data.frame grouped by class standing and
   # number of students in each respective class standing.
   summarized_data <- the_data %>% 
-    group_by(class_standing) %>% 
+    group_by(What.is.your.current.class.standing.) %>% 
     summarise(
       num_students = n()
     )
   
   # Makes bar chart
   plot_ly(summarized_data,
-          x = class_standing, 
+          x = What.is.your.current.class.standing., 
           y = num_students,
           name = "Class Standing Distribution of INFO498F",
           type = "bar"
   ) %>% layout(
     barmode = "stack",
-    title = "Distribution of Class Standing",
+    title = "Distribution of Class Standing for INFO498f",
     xaxis = list(title = "Class Standing"),
     yaxis = list(title = "Number of Students")
   )
