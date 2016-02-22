0d7a751627f5401c27d799f25e320f03e1f75772
EPAHANG <epahang@uw.edu>
2016-02-22 02:44:06 -0800

    Update index.rmd and chartfile1.r

--- scripts/chartfile1.R
+++ scripts/chartfile1.R
@@ -1,11 +1,17 @@
 # Assignment 7 - Chart File 1
-
-# Install packages
-library(jsonlite)
-library(dplyr)
-library(plotly)
+# data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
 
 # Write a function that takes in a dataset as a parameter, and returns a visualization 
-# of that data. This will allow you to call your charting function from your index.Rmd file, 
-# keeping your code much easier to read.
-
+# of that data, allowing you to call your charting function from your index.Rmd file.
+make_INFOinterest_chart <- function(dataset) {
+  p <- dataset %>% count(Are.you.interested.in.applying.to.the.Informatics.major., What.operating.system.do.you.typically.use.) %>%
+    plot_ly(x = Are.you.interested.in.applying.to.the.Informatics.major., 
+            y = n, 
+            type = "bar", 
+            color = What.operating.system.do.you.typically.use.) %>%
+    
+    layout(title = "INFO498F Winter 2016 Class Interest in the Informatics Major & Operating Systems Used",
+            xaxis = list(title = "Interest in Applying to the Informatics Major"),
+            yaxis = list(title = "Number of Students")
+    )
+}
