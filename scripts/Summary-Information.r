cf0182f885f8c27e8f04875b78b8f3ebe6c338e5
Jonathan Ieng <Jonathan Ieng>
2016-02-24 11:32:11 -0800

    fixed errors with spacing

--- scripts/Summary-Information.r
+++ scripts/Summary-Information.r
@@ -111,13 +111,11 @@ info_function <- function(dataset) {
                                       nrow()
   
   ret$num_moderate_programming_exp <- dataset %>% 
-                                        filter(programming_experience == "Moderate experience with a scripting 
-                                                                          language (Python, R, JavaScript, Java, etc.)") %>% 
+                                        filter(programming_experience == "Moderate experience with a scripting language (Python, R, JavaScript, Java, etc.)") %>% 
                                           nrow()
   
   ret$num_high_programmig_exp <- dataset %>% 
-                                  filter(programming_experience == "Lots of experience with a scripting 
-                                                                    language (Python, R, JavaScript, Java, etc.)") %>% 
+                                  filter(programming_experience == "Lots of experience with a scripting language (Python, R, JavaScript, Java, etc.)") %>% 
                                     nrow()
   
   #Total number of countries visited
